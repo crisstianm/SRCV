5dd87df5a6868e67d8c6c39374229c96fcab676f
Crisstian Mella <crisstian6@hotmail.com>
2020-12-16 20:44:02 -0300

    Se inicializa la aplicacion para desarrollar el login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FormLoginComponent } from './component/form-login/form-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FormLoginComponent
   ],
   imports: [
     BrowserModule,
